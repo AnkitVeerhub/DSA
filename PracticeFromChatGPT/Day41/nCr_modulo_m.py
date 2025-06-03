63b6e91eb74f9a3cdf359336e7960e06009d9fe1
AnkitVeerhub <ankitveer54@gmail.com>
2025-06-03 08:58:43 +0530

    Clearing Repo

--- PracticeFromChatGPT/Day41/nCr_modulo_m.py
+++
@@ -1,9 +0,0 @@
-'''
-Problem Description
-Given three integers A, B, and C, where A represents n, B represents r, and C represents m, find and return the value of nCr % m where nCr % m = (n!/((n-r)!*r!))% m.
-x! means factorial of x i.e. x! = 1 * 2 * 3... * x.
-Problem Constraints
-1 <= A * B <= 10^6
-1 <= B <= A
-1 <= C <= 10^6
-'''
